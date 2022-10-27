92117b899e23f163352d046dc63b4d562af5dc8f
Mohit <pipaliyamohit4g@gmail.com>
2022-10-27 21:05:47 +0530

    project ready to deploy

--- task1/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    for(let i=1;i<=10;i++){
-        let h = "#h"+i;
-        let b  = "#b"+i;
-        $(h).click(function(){
-          $(b).toggle();
-        });
-    }
-});
